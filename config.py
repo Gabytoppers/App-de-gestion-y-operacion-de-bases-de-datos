f02edb6daf1d099a97f0c826c1fcb5b03343a0d1
gabriela pereira <gabydev.pereira@gmail.com>
2025-05-19 18:33:58 -0500

    Listo para render 3

--- config.py
+++ config.py
@@ -2,13 +2,16 @@ import os
 from flask_pymongo import PyMongo
 
 class Config:
-    MONGO_URI = os.getenv("MONGO_URI_ATLAS", "mongodb+srv://gabydevpereira:AmYJBPEgBhp8mOAm@cluster0.ans4nre.mongodb.net/stock_app_atlas?retryWrites=true&w=majority&appName=Cluster0"
+    MONGO_URI = os.getenv(
+        "MONGO_URI_ATLAS",
+        "mongodb+srv://gabydevpereira:AmYJBPEgBhp8mOAm@cluster0.ans4nre.mongodb.net/stock_app_atlas?retryWrites=true&w=majority&appName=Cluster0"
     )
-    MONGO_URI_LOCAL = os.getenv("MONGO_URI_LOCAL", "mongodb://localhost:27017/stock_app_local")
-
+    MONGO_URI_LOCAL = os.getenv(
+        "MONGO_URI_LOCAL",
+        "mongodb://localhost:27017/stock_app_local"
+    )
 
 def init_db(app):
     mongo_atlas = PyMongo(app, uri=app.config["MONGO_URI"])
     mongo_local = PyMongo(app, uri=app.config["MONGO_URI_LOCAL"])
     return mongo_atlas, mongo_local
-
